6bcd65a747e9a9b03de04afa4fb09113d2a751ef
sojeonglee <sojeonglee0125@gmail.com>
2022-10-06 22:40:02 +0900

    test: MemberService 메인 메서드에 테스트 작성 , 테스트 코드 작성

--- src/main/java/spring/core/member/MemberServiceImpl.java
+++ src/main/java/spring/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package spring.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
+    // 실제 할당하는 부분이 인터페이스와 구현체를 동시에 의존하고 있다.
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
