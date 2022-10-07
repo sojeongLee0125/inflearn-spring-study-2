195681ff46a4d3ddbb84798c1816eb260bbd740e
sojeonglee <sojeonglee0125@gmail.com>
2022-10-07 23:06:49 +0900

    Refactor: AppConfig 중복 코드 제거 및 역할에 따른 구현 명확하게 변경

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -1,6 +1,8 @@
 package spring.core;
 
+import spring.core.discount.DiscountPolicy;
 import spring.core.discount.FixDiscountPolicy;
+import spring.core.member.MemberRepository;
 import spring.core.member.MemberService;
 import spring.core.member.MemberServiceImpl;
 import spring.core.member.MemoryMemberRepository;
@@ -10,12 +12,20 @@ import spring.core.order.OrderServiceImpl;
 public class AppConfig {
 
     // 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 설정 클래스
+    // 한 생성 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입
     public MemberService memberService() {
-        // 한 생성객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
