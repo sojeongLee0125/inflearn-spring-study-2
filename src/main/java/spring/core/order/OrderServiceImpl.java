264c4caad65cb73fab714e7b824ce381c285a7df
sojeonglee <sojeonglee0125@gmail.com>
2022-10-07 21:11:17 +0900

    Comment: OrderServiceImpl 설명 주석 추가

--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -10,7 +10,10 @@ public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-
+    // 구체 클래스에 동시에 의존하여 할인 정책을 변경하려면 클라이언트인 OrderServiceImpl 코드를 고쳐야 한다 (OCP/DIP 위반)
+    // private final DiscountPolicy discountPolicy; : 인터페이스만 의존하면 NPE 발생
+    // 누군가가 클라이언트인 OrderServiceImpl에 Disco untPolicy 구현 객체를 대신 생성하고 주입해주어야 한다.
+    // 관심사의 분리 듣기
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
