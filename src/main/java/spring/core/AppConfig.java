14827022993c35f037ea0f539daf2044144e4af5
sojeonglee <sojeonglee0125@gmail.com>
2022-10-07 22:02:16 +0900

    feat: 실제 구체 클래스를 결정하는 AppConfig 파일 생성

--- src/main/java/spring/core/AppConfig.java
+++ src/main/java/spring/core/AppConfig.java
@@ -9,7 +9,9 @@ import spring.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 애플리케이션의 전체 동작 방식을 구성(config)하기 위해, 구현 객체를 생성하고, 연결하는 책임을 가지는 설정 클래스
     public MemberService memberService() {
+        // 한 생성객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
 
