e7f25fbefd67372d73f47f3e03eadfb64a6d8e89
sojeonglee <sojeonglee0125@gmail.com>
2022-10-17 20:50:58 +0900

    test : 스프링 컨테이너에서 싱글톤 객체 생성 확인 테스트

--- src/test/java/spring/core/singleton/SingletonTest.java
+++ src/test/java/spring/core/singleton/SingletonTest.java
@@ -2,6 +2,7 @@ package spring.core.singleton;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import spring.core.AppConfig;
 import spring.core.member.MemberService;
 
@@ -26,7 +27,7 @@ public class SingletonTest {
 
     @Test
     @DisplayName("싱글톤 패턴을 적용한 객체 사용")
-    void singletonServiceTest(){
+    void singletonServiceTest() {
         // 호출할 때 마다 같은 객체를 반환
         SingletonService instance1 = SingletonService.getInstance();
         SingletonService instance2 = SingletonService.getInstance();
@@ -41,6 +42,20 @@ public class SingletonTest {
 
         instance1.logic();
         instance2.logic();
+    }
 
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같아.
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
