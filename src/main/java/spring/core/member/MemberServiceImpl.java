588ca9c14b711e38487c8149dfa26df59a06ad5f
sojeonglee <sojeonglee0125@gmail.com>
2022-10-17 22:04:40 +0900

    feat : 객체들에 @Component 애너테이션 추가

--- src/main/java/spring/core/member/MemberServiceImpl.java
+++ src/main/java/spring/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package spring.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -8,6 +12,8 @@ public class MemberServiceImpl implements MemberService {
 
     // 설계 변경으로 MemberServiceImpl은 MemoryMemberRepository 를 의존하지 않는다
     // 단지 MemberRepository 인터페이스만 의존
+
+    @Autowired // ComponentScan 에서 의존관계를 알려주기 위해서 @Autowired 사용
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/spring/core/order/OrderServiceImpl.java
+++ src/main/java/spring/core/order/OrderServiceImpl.java
@@ -1,9 +1,12 @@
 package spring.core.order;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import spring.core.discount.DiscountPolicy;
 import spring.core.member.Member;
 import spring.core.member.MemberRepository;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -17,6 +20,8 @@ public class OrderServiceImpl implements OrderService {
 
     // 설계 변경으로 OrderServiceImpl 은 MemoryMemberRepository, FixDiscountPolicy 를 의존하지 않는다
     // 단지 인터페이스만 의존
+
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
