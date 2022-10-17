359b353471f3f10b9273ef60689fdbdaf1e7d040
sojeonglee <sojeonglee0125@gmail.com>
2022-10-17 22:05:08 +0900

    test : @ComponentScan 을 활용한 스프링 빈 생성 테스트 작성

---
+++ src/test/java/spring/core/scan/AutoAppConfigTest.java
@@ -0,0 +1,19 @@
+package spring.core.scan;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import spring.core.AutoAppConfig;
+import spring.core.member.MemberService;
+
+public class AutoAppConfigTest {
+
+    @Test
+    void basicScan() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
+
+        MemberService memberService = ac.getBean(MemberService.class);
+        Assertions.assertThat(memberService).isInstanceOf(MemberService.class);
+
+    }
+}
