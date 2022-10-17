5507b09e36e9932717edc0817b93f499a2bfe78b
sojeonglee <sojeonglee0125@gmail.com>
2022-10-17 21:51:05 +0900

    test : 스프링 컨테이너 싱글톤 테스트 - @Configuration 애너테이션이 없을 경우 스프링 싱글톤 깨지는 현상 테스트

--- src/test/java/spring/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/spring/core/singleton/ConfigurationSingletonTest.java
@@ -31,4 +31,17 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
 
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // 스프링은 AppConfig를 상속받은 AppConfig$$EnhancerBySpringCGLIB을 사용해서 등록한다.
+        // @Configuration 이 있어야 스프링은 바이트코드를 조작해서 싱글톤을 보장한다.
+        // 그리고 memberRepository도 3번 호출하여 싱글톤이 깨진다.
+        System.out.println("bean = " + bean.getClass());
+
+
+    }
 }
