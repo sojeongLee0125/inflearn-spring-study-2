969b9dc4f3cd86599a716fc9fc68c51bd7da658d
sojeonglee <sojeonglee0125@gmail.com>
2022-10-17 21:05:21 +0900

    feat : 싱글톤에서 공유 필드를 없에고 지역변수를 사용

--- src/test/java/spring/core/singleton/StatefulService.java
+++ src/test/java/spring/core/singleton/StatefulService.java
@@ -1,15 +1,14 @@
 package spring.core.singleton;
 
 public class StatefulService {
-    private int price; // 상태를 유지하는 필드
+    // private int price; // 상태를 유지하는 필드
 
-
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + "price = " + price);
-        this.price = price; // 문제 발생
-    }
-
-    public int getPrice() {
         return price;
     }
+
+//    public int getPrice() {
+//        return price;
+//    }
 }
