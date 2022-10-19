65d1c63bd04d2c70145724fcc4e118fd2d47eedb
sojeonglee <sojeonglee0125@gmail.com>
2022-10-19 20:37:51 +0900

    fix : 중복 빈 존재 에러 수정, 주석 처리

--- src/main/java/spring/core/AutoAppConfig.java
+++ src/main/java/spring/core/AutoAppConfig.java
@@ -18,8 +18,9 @@ public class AutoAppConfig {
     // 동일한 이름의 자동 빈 등록과 수동 빈 등록이 존재하면 수동 빈등록이 우선권을 가진다.
     // 그러나 최근 스프링 부트는 수동 빈등록과 자동 빈등록이 충돌하면 오류가 나도록 기본값을 변경했다.
     // spring.main.allow-bean-definition-overriding=true 설정값을 추가하면 오류가 나지 않도록 할 수 있다.
-    @Bean(name = "memoryMemberRepository")
+
+   /* @Bean(name = "memoryMemberRepository")
     MemberRepository memberRepository(){
         return new MemoryMemberRepository();
-    }
+    }*/
 }
