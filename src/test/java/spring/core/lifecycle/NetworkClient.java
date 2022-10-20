98b977140846933f9b7cf568ae3e90108c619727
sojeonglee <sojeonglee0125@gmail.com>
2022-10-20 20:37:12 +0900

    feat : 인터페이스를 활용한 빈의 초기화 소멸 메서드 작성

---
+++ src/test/java/spring/core/lifecycle/NetworkClient.java
@@ -0,0 +1,51 @@
+package spring.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
+    private String url;
+
+    // 스프링 빈 라이프 사이클 : 객체 생성 후 의존관계 주입 => 필요한 데이터 사용할 수 있는 준비 완료
+    // 초기화는 준비완료 되었을 때 호출됨.
+    // 스프링 컨테이너 생성 -> 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸 전 콜백 -> 스프링 종료
+    // => 객체의 생성과 초기화는 분리하는 게 좋다.
+
+    public NetworkClient() {
+        System.out.println("생성자 호출 url = " + url);
+
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+
+    }
+
+    // 스프링 전용 인터페이스, 스프링에 의존
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+}
